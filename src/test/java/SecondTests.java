f6da3b24ac0ba08fa33cc3dc8fd885d7baafe125
alanc <alcalvillo@irvinecompany.com>
2021-02-09 11:46:32 -0600

    added more tests

---
+++ src/test/java/SecondTests.java
@@ -0,0 +1,47 @@
+import io.restassured.internal.mapping.GsonMapper;
+import io.restassured.path.json.JsonPath;
+import mocks.Courses;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.stream.Collectors;
+
+public class SecondTests {
+
+    @Test
+    public void testWithMocks(){
+        JsonPath js = new JsonPath(Courses.mockCourses());
+
+        int count = js.getInt("courses.size()");
+
+        System.out.println(count);
+
+        int totalAmount = js.getInt("dashboard.purchaseAmount");
+        System.out.println(totalAmount);
+
+        String firstCourse = js.getString("courses[0].title");
+        System.out.println(firstCourse);
+
+       for(int i = 0; i < count; i++){
+           String courseTitle = js.getString("courses["+i+"].title");
+           System.out.println(courseTitle);
+           if(courseTitle.equalsIgnoreCase("RPA")){
+               System.out.println(js.getString("courses["+i+"].copies"));
+           }
+           //System.out.println(js.getString("courses["+i+"].price"));
+       }
+    }
+    @Test
+    public void testSum(){
+        JsonPath js = new JsonPath(Courses.mockCourses());
+        int totalAmount = js.getInt("dashboard.purchaseAmount");
+        System.out.println(totalAmount);
+
+        int totalPrice = 0;
+        for(int i = 0; i < js.getInt("courses.size()"); i++){
+            totalPrice += js.getInt("courses["+i+"].price") * js.getInt("courses["+i+"].copies");
+        }
+        Assert.assertEquals(totalAmount,totalPrice);
+    }
+}
+
